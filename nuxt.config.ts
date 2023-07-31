5b23b8a42e671474e3e2898b3d5d8af0f5f7a78d
ᴹᵃᵈᵉ ᵒⁿ ᴱᵃʳᵗʰ ᵇʸ ʰᵘᵐᵃⁿˢ <39050604+EXPITC@users.noreply.github.com>
2023-07-31 15:23:15 +0700

    chore: move to src
    + nuxt.config.ts
    + app.vue
    + main.css
    + index.vue
    + tailwind.config.js

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  srcDir: 'src/',
   devtools: { enabled: true },
   modules: ['@nuxtjs/eslint-module'],
   css: ['~/assets/css/main.css'],
