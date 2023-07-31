f25a5f8ddef44ba54000e48379f8a3b776fc46ef
ᴹᵃᵈᵉ ᵒⁿ ᴱᵃʳᵗʰ ᵇʸ ʰᵘᵐᵃⁿˢ <39050604+EXPITC@users.noreply.github.com>
2023-07-31 14:11:09 +0700

    feat: init *
    + nuxt
    + eslint
    + tailwind

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex justify-center items-center h-screen bg-red-600">
+    <NuxtPage />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1 class="text-white">
+    Spoiler ⚠︎lert!
+  </h1>
+</template>
