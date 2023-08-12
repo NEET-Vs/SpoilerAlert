71be1d3e7611ae4fa211083196aca83f5b0ec2c5
ᴹᵃᵈᵉ ᵒⁿ ᴱᵃʳᵗʰ ᵇʸ ʰᵘᵐᵃⁿˢ <39050604+EXPITC@users.noreply.github.com>
2023-08-12 22:00:43 +0700

    RESET!: add eslint - transition to pnpm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  srcDir: "src/",
-  modules: ["@chakra-ui/nuxt-next"],
+  srcDir: 'src/',
+  modules: ['@chakra-ui/nuxt-next', '@nuxtjs/eslint-module'],
   chakra: {
     extendTheme: {
       colors: {
         brand: {
-          whitee: "#fff",
-        },
-      },
-    },
+          whitee: '#fff'
+        }
+      }
+    }
   },
-  devtools: { enabled: true },
-});
+  devtools: { enabled: true }
+})
